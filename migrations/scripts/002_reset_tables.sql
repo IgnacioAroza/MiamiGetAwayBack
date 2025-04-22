0f3a827fc59557bb4bf979410c668585ac0005f9
Ignacio <ignacioaroza.ia@gmail.com>
2025-04-22 16:28:08 -0300

    add scripts to migration: reset_table to delete info

--- migrations/scripts/002_reset_tables.sql
+++ migrations/scripts/002_reset_tables.sql
@@ -4,10 +4,12 @@
 -- Eliminar datos de reservations y tablas relacionadas
 TRUNCATE TABLE reservation_payments CASCADE;
 TRUNCATE TABLE reservations CASCADE;
+TRUNCATE TABLE monthly_summaries CASCADE;
 
 -- Resetear las secuencias de ID a 1
 ALTER SEQUENCE reservations_id_seq RESTART WITH 1;
 ALTER SEQUENCE reservation_payments_id_seq RESTART WITH 1;
+ALTER SEQUENCE monthly_summaries_id_seq RESTART WITH 1;
 
 -- Log completado
 SELECT 'Reset de tablas completado. Todos los datos han sido eliminados.' as message; 
